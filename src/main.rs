4e0b5a624b0e2d74c59f155cbe20772563ddcde5
Max Giga <gigamaximwachau@gmail.com>
2023-10-15 13:17:50 +0300

    add message when no input files are provided

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{error::Error, fs};
 use tu4::Machine;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let filename = std::env::args().nth(1).unwrap();
+    let filename = std::env::args().nth(1).expect("no input files");
     let mut machine: Machine = fs::read_to_string(filename)?.parse()?;
     let input = std::io::stdin().lines().next().unwrap()?;
     machine.set_input(&input);
